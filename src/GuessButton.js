f4122c20d3f89066e7dd6b965ac90b2c22a7614b
John Roberts <bigjlr2004@yahoo.com>
2024-03-12 10:02:18 -0500

    first commit

---
+++ src/GuessButton.js
@@ -0,0 +1,14 @@
+function GuessButton() {
+  function handleClick() {
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNum);
+    let userInput = prompt("type a number");
+    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+  }
+
+  return (
+    <button onClick={handleClick}>Guess the number betwen 1 and 3.</button>
+  );
+}
+
+export default GuessButton;
